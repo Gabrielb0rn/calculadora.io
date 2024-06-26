cfeb35077dd58ebfa632aa4ea02ba4f26c625d6e
Gabrielb0rn <gabriel_born@estudante.sesisenai.org.br>
2024-06-26 12:14:22 -0300

    Atualização

---
+++ .history/assets/scripts_20240626121415.js
@@ -0,0 +1,14 @@
+//dasdadasda
+
+
+
+let elementoInputUm = document.getElementById('nome')
+let elementoInputDois = document.getElementById('idade')
+
+function mostrarInformacoes(){
+    alert(elementoInputUm.value + ' ' + elementoInputDois.value)
+}
+
+function botao() {
+    alert('Teste')
+}
